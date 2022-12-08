2c5e1b458022fb9f4806209f94ff39646ed0ab9e
Colton <colton1ward@gmail.com>
2022-12-08 05:47:54 -0600

    Finished stage 5

--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -26,45 +26,35 @@ public class Main {
    */
 
   public static void main(String[] args) {
-    Battlefield battlefield = new Battlefield();
-    battlefield.display();
+    Battlefield player1Battlefield = new Battlefield();
+    Battlefield player2Battlefield = new Battlefield();
 
-    placeShip(battlefield, new Ship("Aircraft Carrier", 5));
-    placeShip(battlefield, new Ship("Battleship", 4));
-    placeShip(battlefield, new Ship("Submarine", 3));
-    placeShip(battlefield, new Ship("Cruiser", 3));
-    placeShip(battlefield, new Ship("Destroyer", 2));
+    System.out.println("Player 1, place your ships on the game field:");
+    placeShips(player1Battlefield);
+    passToOtherPlayer();
 
-    startGame(battlefield);
+    System.out.println("Player 2, place your ships to the game field:");
+    placeShips(player2Battlefield);
+    passToOtherPlayer();
+
+    startGame(player1Battlefield, player2Battlefield);
   }
 
-  public static void startGame(Battlefield battlefield) {
-    System.out.println("The game starts!");
-    battlefield.display(true);
-    System.out.println("Take a shot!");
+  public static void startGame(Battlefield player1Battlefield, Battlefield player2Battlefield) {
     while (true) {
-      String line = s.nextLine().toUpperCase();
-      Coordinate coordinate = new Coordinate(line);
-      try {
-        TargetStatus status = battlefield.shootTarget(coordinate);
-        battlefield.display(true);
-        if (status.type == Type.HIT) {
-          if (status.ship.getHasSunk()) {
-            if (battlefield.getNumShipsLeft() == 0) {
-              System.out.println("You sank the last ship. You won. Congratulations!");
-              return;
-            } else {
-              System.out.println("You sank a ship! Specify a new target:");
-            }
-          } else {
-            System.out.println("You hit a ship! Try again:");
-          }
-        } else {
-          System.out.println("You missed!");
-        }
-      } catch (IllegalArgumentException e) {
-        System.out.println(e.getMessage());
+      displayBattlefields(player1Battlefield, player2Battlefield);
+      System.out.println("Player 1, it's your turn:");
+      if (takeTurn(player2Battlefield)) {
+        break;
       }
+      passToOtherPlayer();
+
+      displayBattlefields(player2Battlefield, player1Battlefield);
+      System.out.println("Player 2, it's your turn:");
+      if (takeTurn(player1Battlefield)) {
+        break;
+      }
+      passToOtherPlayer();
     }
   }
 
@@ -88,5 +78,53 @@ public class Main {
       break;
     }
     battlefield.display();
+  }
+
+  public static void placeShips(Battlefield battlefield) {
+    battlefield.display();
+    placeShip(battlefield, new Ship("Aircraft Carrier", 5));
+    placeShip(battlefield, new Ship("Battleship", 4));
+    placeShip(battlefield, new Ship("Submarine", 3));
+    placeShip(battlefield, new Ship("Cruiser", 3));
+    placeShip(battlefield, new Ship("Destroyer", 2));
+  }
+
+  public static void passToOtherPlayer() {
+    System.out.println("Press Enter and pass the move to another player");
+    s.nextLine();
+  }
+
+  public static boolean takeTurn(Battlefield foeBattlefield) {
+    while (true) {
+      String line = s.nextLine().toUpperCase();
+      Coordinate coordinate = new Coordinate(line);
+      try {
+        TargetStatus status = foeBattlefield.shootTarget(coordinate);
+        if (status.type == Type.HIT) {
+          if (status.ship.getHasSunk()) {
+            if (foeBattlefield.getNumShipsLeft() == 0) {
+              System.out.println("You sank the last ship. You won. Congratulations!");
+              return true;
+            } else {
+              System.out.println("You sank a ship!");
+            }
+          } else {
+            System.out.println("You hit a ship!");
+          }
+        } else {
+          System.out.println("You missed!");
+        }
+        return false;
+      } catch (IllegalArgumentException e) {
+        System.out.println(e.getMessage());
+      }
+    }
+  }
+
+  public static void displayBattlefields(Battlefield friendlyBattlefield,
+      Battlefield foeBattlefield) {
+    foeBattlefield.display(true);
+    System.out.println("----------------------");
+    friendlyBattlefield.display();
   }
 }
